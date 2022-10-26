0578e4d585dfeb28f952ea7bd44ae34469ff1851
mslep <v-mslepko@expediagroup.com>
2022-10-26 17:26:19 +0300

    Error handled, admin pass filtered

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+
+// /admin/add-product => GET request
 router.get('/add-product', (req, res, next) => {
     console.log("In the add-product");
-    res.send('<form action="/product" method="POST"><input type="text" name="title" ><button type="submit" >Send data</button></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title" ><button type="submit" >Send data</button></form>');
 })
 
+// /admin/add-product => POST request
 router.post('/product', (req, res, next) => {
     console.log(req.body);
     console.log("In the /product");

95c95c321fd18c92bdf6d95072abbd1d00361689
mslep <v-mslepko@expediagroup.com>
2022-11-09 17:07:44 +0200

    fix(no-jira): custom refactor

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
 
 app.use(
 	bodyParser.urlencoded({

353d56f8dd9e3ecad462b4212b8b16342067f6d5
mslep <v-mslepko@expediagroup.com>
2022-11-15 16:14:22 +0200

    fix(no-jira): lint-staged logic fix-try

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
 	host: 'localhost',
 	user: 'root',
 	database: 'node-complete',
-	password: 'Kinureninaza13+'
+	password: 'Kinureninaza13+',
 });
 
 module.exports = pool.promise();
